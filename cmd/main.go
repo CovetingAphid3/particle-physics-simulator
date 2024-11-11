b4d8adffdc63ed17c918c60fb21d5350b36fed82
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-11-12 00:32:01 +0200

    reduce particle to one

--- cmd/main.go
+++ cmd/main.go
@@ -9,8 +9,8 @@ import (
 func main() {
     // Create some test particles
     particles := []*particle.Particle{
-        {X: 100, Y: 500, Radius: 10, Mass: 1, Color: particle.Color{R: 1, G: 0, B: 0, A: 1}},
-        {X: 200, Y: 200, Radius: 10, Mass: 1, Color: particle.Color{R: 0, G: 1, B: 0, A: 1}},
+        {X: 0, Y: 500, Radius: 10, Mass: 1, Color: particle.Color{R: 1, G: 0, B: 0, A: 1}},
+        // {X: 0, Y: 200, Radius: 10, Mass: 1, Color: particle.Color{R: 0, G: 1, B: 0, A: 1}},
     }
 
     // Run the simulation
