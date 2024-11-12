d9cdc0ba64f4b292872776194652bf0487bb011a
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-11-12 19:02:20 +0200

    make sure collisions are working properly

--- internal/simulation/simulation.go
+++ internal/simulation/simulation.go
@@ -15,39 +15,40 @@ const TimeStep = 1.0 / 60.0 // 60 FPS
 
 // Run the simulation
 func RunSimulation(particles []*particle.Particle) {
-	renderer.InitWindow()
-	defer renderer.CloseWindow()
+    renderer.InitWindow()
+    defer renderer.CloseWindow()
 
-	for !rl.WindowShouldClose() {
-		// Update particle physics
-		for _, p := range particles {
-			physics.ApplyGravity(p)
-			physics.UpdateVelocity(p, TimeStep)
-			physics.UpdatePosition(p, TimeStep)
-		}
+    for !rl.WindowShouldClose() {
+        // Update particle physics
+        for _, p := range particles {
+            physics.ApplyGravity(p)
+            physics.UpdateVelocity(p, TimeStep)
+            physics.UpdatePosition(p, TimeStep)
+        }
 
-		// Check for collisions between particles using swept collision detection
-		for i := 0; i < len(particles); i++ {
-			for j := i + 1; j < len(particles); j++ {
-				if collisions.WillCollide(particles[i], particles[j], TimeStep) || collisions.CheckCollision(particles[i],particles[j]) {
-					collisions.HandleCollision(particles[i], particles[j])
-				}
-			}
-		}
+        // Check for collisions using swept collision detection
+        for i := 0; i < len(particles); i++ {
+            for j := i + 1; j < len(particles); j++ {
+                if collisions.WillCollide(particles[i], particles[j], TimeStep) {
+                    collisions.HandleCollision(particles[i], particles[j])
+                }
+            }
+        }
 
-		// Render particles
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.RayWhite)
+        // Render particles
+        rl.BeginDrawing()
+        rl.ClearBackground(rl.RayWhite)
 
-		for _, p := range particles {
-			renderer.DrawParticle(p)
-		}
+        for _, p := range particles {
+            renderer.DrawParticle(p)
+        }
 
-		rl.EndDrawing()
+        rl.EndDrawing()
 
-		time.Sleep(time.Millisecond * 16) // Simulate 60 FPS
-	}
+        time.Sleep(time.Millisecond * 16) // Simulate 60 FPS
+    }
 }
+
 
 func RunSimulationSingle(p *particle.Particle) {
 	RunSimulation([]*particle.Particle{p})
