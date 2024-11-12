53a5cbb6b918b767aa64e12be4599531fd0a1c3a
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-11-12 11:30:08 +0200

    remove main binary

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ func main() {
     // Create some test particles
     particles := []*particle.Particle{
         {X: 0, Y: 500, Radius: 10, Mass: 1, Color: particle.Color{R: 1, G: 0, B: 0, A: 1}},
-        // {X: 0, Y: 200, Radius: 10, Mass: 1, Color: particle.Color{R: 0, G: 1, B: 0, A: 1}},
+        {X: 300, Y: 200, Radius: 10, Mass: 1, Color: particle.Color{R: 0, G: 1, B: 0, A: 1}},
     }
 
     // Run the simulation
